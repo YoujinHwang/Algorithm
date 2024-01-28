7fe6c8c677ddfba2bdf1ee028efadffecfbc364b
jinyouh <rgh747052@gmail.com>
2024-01-29 01:14:20 +0900

    [Silver III] Title: 두 수의 합, Time: 36 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -2,7 +2,6 @@
 #include<algorithm>
 using namespace std;
 int arr[2000001];
-int arr2[1000000];
 
 int main() {
 	int n,x,cnt=0;
@@ -12,20 +11,15 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> a;
 		arr[a] = 1;
-		arr2[i] = a;
 	}
 	cin >> x;
-/*	for (int i = 1; i < x / 2; i++) {
+	for (int i = 1; i < x/2+1; i++) {
 		int num = x - i;
 		if (arr[i] == 1 && arr[num] == 1) {
-			cnt++;
+            if(i!=num)
+			    cnt++;
 
 		}
-	}*/
-	for (int i = 0; i < n; i++) {
-		int num = x - arr2[i];
-		if (arr[num] == 1)
-			cnt++;
 	}
-	cout << cnt/2;
+	cout << cnt;
 }
