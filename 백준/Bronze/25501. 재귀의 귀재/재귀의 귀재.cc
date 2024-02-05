ffda793588dd9c47c5c5ca75af5c42849c05721b
jinyouh <rgh747052@gmail.com>
2024-02-05 18:43:14 +0900

    Update 재귀의 귀재.cc
    오답 이유 기재

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.cc
@@ -3,7 +3,9 @@ using namespace std;
 int cnt;
 
 
-int recursion(const string& s, int l, int r) {
+int recursion(const string& s, int l, int r) { //그냥 string s으로 할 경우 시간초과. 이는 재귀 호출마다
+						// 문자열의 복사본을 생성함. 따라서 큰 문자열과 깊은 재귀에 대해서는 상당한 오버헤드를 발생시킬 수 있음.
+						// 문자열을 참조로 전달하여 이 문제를 해결할 수 있다.
 	if (l >= r) {
 		return 1;
 	}
