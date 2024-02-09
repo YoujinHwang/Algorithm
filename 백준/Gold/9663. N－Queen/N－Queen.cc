51afd34625489c6db3e7ec14b2c637535f3b4104
jinyouh <rgh747052@gmail.com>
2024-02-09 18:40:18 +0900

    [Gold IV] Title: N-Queen, Time: 1688 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[15][15];
+bool isHere[15];
+
+int N;
+int cnt;
+bool diag1[30], diag2[30]; // 대각선 체크
+
+void place(int k) {
+	if (k == N) {
+		cnt++;
+		return;
+	}
+	for (int j = 0; j < N; j++) {
+		if (!isHere[j] && !diag1[k + j] && !diag2[k - j + N - 1]) {
+			isHere[j] = diag1[k + j] = diag2[k - j + N - 1] = true;
+			place(k + 1);
+			isHere[j] = diag1[k + j] = diag2[k - j + N - 1] = false;
+		}
+	}
+}
+
+
+
+int main() {
+	cin >> N;
+	place(0);
+	cout << cnt;
+	return 0;
+}
