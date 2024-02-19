e7c7ef6e5813e9f905b03b676a5c495ac8ec476c
jinyouh <rgh747052@gmail.com>
2024-02-20 00:27:24 +0900

    [Silver II] Title: 이항 계수 2, Time: 4 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int d[1001][1001];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, k;
+	cin >> n >> k;
+	d[0][0] = 1;
+	d[1][0] = 1;
+	d[1][1] = 1;
+	for (int i = 2; i <= n; i++) {
+		for (int j = 0; j <= i; j++) {
+			if (j == 0) d[i][j] = 1;
+			else {
+				d[i][j] = d[i - 1][j - 1]%10007 + d[i - 1][j]%10007;
+				d[i][j]=d[i][j] % 10007;
+			}
+		}
+        d[i][i] = 1;
+	}
+	cout << d[n][k];
+}
