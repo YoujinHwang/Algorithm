99b1d67e4589ead089ec92f023839b6536e33680
YoujinHwang <rgh747052@gmail.com>
2024-03-11 19:00:15 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 2.64 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,6 +1,6 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 18:59:43
+2024년 03월 11일 19:00:11
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -2,61 +2,31 @@
 #include <vector>
 #include <sstream>
 #include <iostream>
-#include <map>
-#include <algorithm>
+
 using namespace std;
-
-map<string, int> name2idx;
-map<string, int> name2factor;
-
-
 int arr[50][50];
 
-
-void init(vector<string> &friends) {
-    int idx=0;
-    for(string f:friends) {
-        name2idx[f]=idx++;
-    }
-}
-bool cmp(int a, int b) {
-    return a>b;
-}
-
 int solution(vector<string> friends, vector<string> gifts) {
-   init(friends);
-    vector<int> v(friends.size(),0);
-    for(auto gift:gifts) {
-        istringstream iss(gift);
-        string name, name2;
-        iss>>name>>name2;
-        name2factor[name]++;
-        name2factor[name2]--;
-        arr[name2idx[name]][name2idx[name2]]++;
-    }
-    for(int i=0;i<friends.size();i++) {
-        for (int j=i+1;j<friends.size();j++) {
-            string name=friends[i];
-            string name2=friends[j];
-            
-            if(arr[i][j]>arr[j][i]) {
-                v[i]++;
+   int size=friends.size();
+    for(auto a:gifts) {
+        vector<string> names;
+        istringstream iss(a);
+        string token;
+        while(getline(iss,token,' ')) {names.push_back(token);}
+        int giver=-1;
+        int get=-1;
+        for(int i=0;i<size;i++) {
+            if(friends[i]==names[0]) {
+                giver=i;
             }
-            else if(arr[i][j]<arr[j][i]) {
-                v[j]++;
-            }
-            else {
-                if(name2factor[name]>name2factor[name2]) {
-                    v[i]++;
-                }
-                else if(name2factor[name]<name2factor[name2]) {
-                    v[j]++;
-                }
+            else if(friends[i]==names[1]) get=i;
+            if(giver!=-1&&get!=-1) {
+                arr[giver][get]++;
+                break;
             }
         }
+       // cout<<names[0]<<' '<<names[1]<<'\n';
     }
-    sort(v.begin(),v.end(),cmp);
     int answer = 0;
-    answer=v[0];
     return answer;
 }
