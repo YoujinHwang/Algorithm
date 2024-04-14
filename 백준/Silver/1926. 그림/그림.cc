60693733d487baf56196321e4caf36963097ae13
YoujinHwang <rgh747052@gmail.com>
2024-04-14 11:16:11 +0900

    [Silver I] Title: 그림, Time: 20 ms, Memory: 3240 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,55 +1,51 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
 int arr[500][500];
-int vis[500][500];
+bool vis[500][500];
+queue<pair<int,int>> q;
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+#define X first
+#define Y second
 
 int main() {
-	int n, m,cnt=0, max;
-	vector<int> area;
-	queue<pair<int,int>> q;
-	cin >> n >> m;
-	int dx[4] = {1,-1,0,0};
-	int dy[4] = { 0,0,1,-1 };
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> arr[i][j];
-		}
-	}
-//	vis[0][0] = 1;
-//	q.push({ 0,0 });
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-//			vis[i][j] = 1;
-			if (vis[i][j]!=1 && arr[i][j] == 1) {
-				cnt++;
-				q.push({ i,j });
-				int a = 1;
-				vis[i][j] = 1;
-				while (!q.empty()) {
-					pair<int, int> cur = q.front();
-					q.pop();
-					
-					for (int i = 0; i < 4; i++) {
-						int nx = cur.first + dx[i];
-						int ny = cur.second + dy[i];
-						if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-//						if (arr[nx][ny]) {
-							if (!vis[nx][ny]&&arr[nx][ny]) {
-								q.push({ nx,ny });
-								vis[nx][ny] = 1;
-								a++;
-							}
-//						}
-					}
-				}
-				area.push_back(a);
-			}
-		}
-	}
-	if (cnt!=0)
-		max = *max_element(area.begin(), area.end());
-	else max = 0;
-
-	
-	cout << cnt <<'\n'<< max<<'\n';
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            cin>>arr[i][j];
+        }
+    }
+    int num=0;
+    int mx=0;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            int tmp=0;
+            if(arr[i][j]&&!vis[i][j]) {
+                tmp++;
+                q.push({i,j});
+                vis[i][j]=1;
+                num++;
+            }
+            while(!q.empty()) {
+                pair<int,int> cur=q.front();
+                q.pop();
+                for(int x=0;x<4;x++) {
+                    int nx=cur.X+dx[x];
+                    int ny=cur.Y+dy[x];
+                    if(nx<0||ny<0||nx>=n||ny>=m) continue;
+                    if(!vis[nx][ny]&&arr[nx][ny]) {
+                        q.push({nx,ny});
+                        vis[nx][ny]=1;
+                        tmp++;
+                    }
+                }
+            }
+            mx=max(mx, tmp);
+        }
+    }
+    cout<<num<<'\n'<<mx;
 }
