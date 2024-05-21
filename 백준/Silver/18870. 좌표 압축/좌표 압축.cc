eb8ccaa51f7cd72ad4d6ca0091a33bc0bab20651
YoujinHwang <rgh747052@gmail.com>
2024-05-21 10:09:27 +0900

    [Silver II] Title: 좌표 압축, Time: 468 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cc
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -1,30 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[1000000];
-vector<int> b;
-int c[1000000];
+int arr1[1000000];
+int arr2[1000000];
+
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-		c[i] = a[i];
-	}
-	sort(a, a + n);
-	int j = 1,x;
-	b.push_back(a[0]);
-	for (int i = 1; i < n; i++) {
-		//x = a[i];
-		if (a[i] != a[i - 1]) {
-			b.push_back(a[i]);
-		}
-		
-	}
-	//for (int i = 0; i < b.size(); i++) cout << b[i] << ' ';
-	for (int i = 0; i < n; i++) {
-		cout<<lower_bound(b.begin(),b.end(),c[i])-b.begin()<<' ';
-	}
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        cin>>arr1[i];
+        arr2[i]=arr1[i];
+    }
+    sort(arr2,arr2+n);
+    vector<int> vv;
+    vv.push_back(arr2[0]);
+    for(int i=1;i<n;i++) {
+        if(arr2[i]!=arr2[i-1]) {
+            vv.push_back(arr2[i]);
+        }
+    }
+    // for(auto s:vv) cout<<s<<' ';
+    // cout<<'\n';
+
+    for(int i=0;i<n;i++) {
+        long long st=0;
+        long long en=vv.size();
+        int ans;
+        while(st<en) {
+            long long mid=(st+en)/2;
+            if(vv[mid]>=arr1[i]) {
+                en=mid;
+            }
+            else if(vv[mid]<arr1[i]) {
+                st=mid+1;
+            }
+            ans=en;
+        }
+        cout<<ans<<' ';
+    }
+    
+
 }
