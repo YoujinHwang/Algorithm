e177d3d37b92584711a011b1028da8723303d867
YoujinHwang <rgh747052@gmail.com>
2024-05-29 23:57:06 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>n;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n-i;j++) {
+            cout<<"*";
+        }
+        cout<<'\n';
+    }
+}
