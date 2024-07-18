001ab4d1badc79f4d3d26da2488060f59b7da230
YoujinHwang <rgh747052@gmail.com>
2024-07-18 16:44:21 +0900

    Update 수강신청.cc
    주의할점

--- 백준/Silver/13414. 수강신청/수강신청.cc
+++ 백준/Silver/13414. 수강신청/수강신청.cc
@@ -29,7 +29,7 @@ int main() {
 		v.push_back(s);
 	}
 	sort(v.begin(), v.end(), compare);
-	for (int i = 0; i < min(K,(int)v.size()); i++) {
+	for (int i = 0; i < min(K,(int)v.size()); i++) { //수강신청 정원보다 더 적게 신청할 수 있으므로 min 해줘야함!!
 		cout << v[i].first << '\n';
 	}
 }
