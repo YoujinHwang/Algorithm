25710750b9a9228e734c5ead43f853f0b04b9f5f
YoujinHwang <rgh747052@gmail.com>
2024-07-18 16:42:15 +0900

    [Silver III] Title: 수강신청, Time: 272 ms, Memory: 35432 KB -BaekjoonHub

---
+++ 백준/Silver/13414. 수강신청/수강신청.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+bool compare(const pair<string, int>& a, const pair<string, int>& b) {
+	return a.second < b.second;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int K, L;
+	cin >> K >> L;
+	unordered_map<string,int> m;
+	for (int i = 0; i < L; i++) {
+		string a;
+		cin >> a;
+		if (m.find(a) != m.end()) {
+			m.erase(a);
+			m.insert({a,i});
+		}
+		else {
+			m.insert({ a,i });
+		}
+	}
+	vector<pair<string, int>> v;
+	for (auto s : m) {
+		v.push_back(s);
+	}
+	sort(v.begin(), v.end(), compare);
+	for (int i = 0; i < min(K,(int)v.size()); i++) {
+		cout << v[i].first << '\n';
+	}
+}
