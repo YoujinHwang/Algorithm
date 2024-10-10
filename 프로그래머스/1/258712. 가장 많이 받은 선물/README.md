10c17da1b2f486ade4a62573f683f1ef3875504f
YoujinHwang <rgh747052@gmail.com>
2024-10-10 10:47:02 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 2.53 ms, Memory: 4.63 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 2.64 ms
+메모리: 4.63 MB, 시간: 2.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 19:00:11
+2024년 10월 10일 10:46:59
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.cpp
@@ -1,32 +1,51 @@
 #include <string>
 #include <vector>
-#include <sstream>
 #include <iostream>
-
+#include <unordered_map>
+#include <algorithm>
 using namespace std;
+unordered_map<int,int> giftIndex; //선물지수
+vector<int> giftNum; //받는 선물 개수
 int arr[50][50];
+unordered_map<string,int> FtoI; //이름과 인덱스 매칭
+
 
 int solution(vector<string> friends, vector<string> gifts) {
-   int size=friends.size();
-    for(auto a:gifts) {
-        vector<string> names;
-        istringstream iss(a);
-        string token;
-        while(getline(iss,token,' ')) {names.push_back(token);}
-        int giver=-1;
-        int get=-1;
-        for(int i=0;i<size;i++) {
-            if(friends[i]==names[0]) {
-                giver=i;
+    int answer = 0;
+    int n=friends.size();
+    int ind=0;
+    for(auto s:friends) {
+        FtoI[s]=ind;
+        giftIndex[ind]=0;
+        ind++;
+    }
+    for(auto s:gifts) {
+        auto it=s.find(' ');
+        string giver=s.substr(0,it);
+        string receiver=s.substr(it+1);
+        //cout<<giver<<' '<<receiver<<'\n';
+        int x=FtoI[giver];
+        int y=FtoI[receiver];
+        arr[x][y]++;
+        giftIndex[x]++;
+        giftIndex[y]--;
+    }
+    for(int i=0;i<n;i++) {
+        int cnt=0;
+        for(int j=0;j<n;j++) {
+            if(i==j) continue;
+            if(arr[i][j]>arr[j][i]) {
+                cnt++;
             }
-            else if(friends[i]==names[1]) get=i;
-            if(giver!=-1&&get!=-1) {
-                arr[giver][get]++;
-                break;
+            else if(arr[i][j]==arr[j][i]) {
+                if(giftIndex[i]>giftIndex[j]) {
+                    cnt++;
+                }
             }
         }
-       // cout<<names[0]<<' '<<names[1]<<'\n';
+        giftNum.push_back(cnt);
     }
-    int answer = 0;
+    sort(giftNum.begin(),giftNum.end(),greater());
+    answer=giftNum[0];
     return answer;
 }
