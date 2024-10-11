1c5c03c1253384da7f182f04124c3d1939a91910
YoujinHwang <rgh747052@gmail.com>
2024-10-11 17:23:54 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,24 @@
+-- 코드를 작성해주세요
+with CTE as (
+select emp_no, case 
+    when sum(score)/2>=96 then 'S'
+when sum(score)/2>=90 then 'A'
+when sum(score)/2 >=80 then'B'
+else 'C' 
+end GRADE
+FROM hr_grade
+group by emp_no
+)
+
+select a.emp_no, a.emp_name, GRADE, case 
+when GRADE='S' then sal*0.2 
+when GRADE ='A' then sal*0.15
+when GRADE ='B' then sal*0.1
+else 0
+end BONUS
+from hr_employees a
+join CTE b
+on a.emp_no=b.emp_no
+join hr_department c
+on a.dept_id=c.dept_id
+order by 1
