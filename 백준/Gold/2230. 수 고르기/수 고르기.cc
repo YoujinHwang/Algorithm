e6744c62e902ff56df9daa53fd03fe69391d214d
YoujinHwang <rgh747052@gmail.com>
2024-10-29 20:40:10 +0900

    [Gold V] Title: 수 고르기, Time: 16 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -1,25 +1,31 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-int n, m;
-int a[100000];
-
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) cin >> a[i];
-	sort(a, a + n);
-	int st=0, en = 0;
-	int ans = INT_MAX;
-	while (en < n) {
-		int dif = a[en] - a[st];
-		if (dif >= m) {
-			ans = min(ans, dif);
-			st++;
-		}
-		else en++;
-	}
-	cout << ans << '\n';
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,m;
+    cin>>n>>m;
+    int arr[100000];
+    for(int i=0;i<n;i++) {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    int l=0;
+    int r=0;
+    int ans=2000000000;
+    while(r<n) {
+        int dif=arr[r]-arr[l];
+        if(dif>=m) {
+            ans=min(ans,dif);
+            l++;
+            
+        }
+        else {
+            r++;
+        }
+    }
 
+    cout<<ans;
 }
